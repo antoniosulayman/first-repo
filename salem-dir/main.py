a17a1440e8fa9c3ff9d0eb265054cca91687b89e
khalifa malid <khalifamalid@hotmail.com>
2025-11-19 02:27:23 -0500

    salem modified

--- salem-dir/main.py
+++ salem-dir/main.py
@@ -1,5 +1,6 @@
-l = [1, 2, 3, 4, 5]
-l.reverse()
-print(l)
-
-
+def my_reverse (l:list):
+   for i in range (len(l)//2):
+      l[i],l[-i-1] = l [-i-1],[i]
+1 = [1,2,3,4,5,6,7,8,]
+my_reverse(1)
+print(1)
