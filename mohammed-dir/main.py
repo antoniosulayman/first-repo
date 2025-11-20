494f3aa71c68889b32442e766094b1282d161d7e
Antoniosulayman2025 <antoniosulayman@outlook.sa>
2025-11-20 02:56:56 -0500

    Update main.py

--- mohammed-dir/main.py
+++ mohammed-dir/main.py
@@ -1,6 +1,15 @@
-l=[1,2,3,4,5]
+def get_max_number(numbers):
+ 
+ my_list = [10, 25, 3, 99, 42]
 
-for l in reversed(l):
-    print(l)
 
-print("Mohammed good man")
+if not _max_numbers():
+    return get_max_number
+    max_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+   
+
+
+print( get_max_number(my_list))
