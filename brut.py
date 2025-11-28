71a760651c4e68db4b7886f571c8f9f0a35e733f
antoniosulayman <antoniosulayman@outlook.sa>
2025-11-28 11:33:57 +0300

    Create brut.py

---
+++ brut.py
@@ -0,0 +1,13 @@
+from rarfile import RarFile, RarityError
+
+passwords = ['1234', 'password', 'admin', '1111']
+
+with RarFile('secrets.rar') as rf:
+    for pwd in passwords:
+        rf.setpassword(pwd)
+        try:
+            rf.extractall()
+            print('Found password!!!: ', pwd)
+            break
+        except RarityError:
+            pass
